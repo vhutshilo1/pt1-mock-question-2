53a51d666e4a582d2d654033da46daa8d66ed976
vhutshilo1 <KharivheVhutshilo@live.com>
2016-03-25 07:17:55 +0200

    creating class and its member functions

---
+++ Time.h
@@ -0,0 +1,27 @@
+
+#ifndef TIME_H
+#define TIME_H
+#include<iostream>
+using namespace std;
+//class Time declaration
+class Time
+{
+private:
+	//declaration of privaate data members.
+	int Hours; //These could be declared in one line like so,
+	int Mins; //int Hours,Mins;
+
+public:
+	//declaration of public members(member functions).
+
+	Time(int hours = 0, int mins = 0);//Declare constructor(with default values)
+	~Time();						   // and Destructor.
+
+	void set(int, int);				//mutator function declaration.
+	int get(int, int);				//accessor function.
+
+	friend ostream& operator<<(ostream&, const Time&);
+	Time operator++(int);
+
+};                                 
+#endif
